b906b58dd53b8780807e6d664c739e3e4be78651
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-06-27 03:27:33 +0545

    Add Hello World Again package

---
+++ src/helloworldagain/Driver.java
@@ -0,0 +1,23 @@
+package helloworldagain;
+
+/**
+ * Prints a welcome message by defining a method to return the message.
+ */
+public class Driver {
+    /**
+     * Main method that prints out a welcome message.
+     */
+    public static void main(String[] args) {
+        System.out.println(getMessage());    // Prints the string returned from 'getMessage()'
+    }    
+
+    /**
+     * Returns the welcome message.
+     * 
+     * @return The welcome message.
+     */
+    public static String getMessage() {
+        String message = "Hello, World!\nWelcome again to Java Mini Projects.";
+        return message;
+    }
+}
