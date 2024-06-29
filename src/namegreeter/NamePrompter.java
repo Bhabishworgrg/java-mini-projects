42c8273b311b6defa03c8679a680188adbffa25c
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-06-30 02:31:02 +0545

    Modify NamePrompter

--- src/namegreeter/NamePrompter.java
+++ src/namegreeter/NamePrompter.java
@@ -1,6 +1,6 @@
 package namegreeter;
 
-import java.util.Scanner;                           // Imports Scanner class
+import java.util.Scanner;                           // Imports Scanner class.
 
 /**
  * Provides a method to get name from the user
@@ -12,11 +12,11 @@ public class NamePrompter {
      * @return The user's name
      */
     public String getName() {
-        Scanner scanner = new Scanner(System.in);   // Creates a Scanner object
+        Scanner scanner = new Scanner(System.in);   // Creates a Scanner object.
 
         System.out.print("What's your name? ");
-        String name = scanner.nextLine();           // Prompts for name from the user
-        scanner.close();                            // Closes the Scanner object to free up the resources
+        String name = scanner.nextLine();           // Prompts for name from the user.
+        scanner.close();                            // Closes the Scanner object to free up the resources.
 
         return name;
     }
