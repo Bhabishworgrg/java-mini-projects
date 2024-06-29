346360c3706ca9278261d050600f655e28c8f0ad
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-06-30 00:53:06 +0545

    Modify name greeter package

--- src/namegreeter/Driver.java
+++ src/namegreeter/Driver.java
@@ -1,7 +1,5 @@
 package namegreeter;
 
-import java.util.Scanner;                           // Imports Scanner class
-
 /**
  * Asks for user's name and greets them.
  */
@@ -10,12 +8,10 @@ public class Driver {
      * Main method that declares two String variables and prints out a welcome message.
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);   // Creates a Scanner object
+        NamePrompter namePrompter = new NamePrompter();
 
-        System.out.print("What's your name? ");
-        String name = scanner.nextLine();           // Prompts for an input with the user
-        scanner.close();                            // Closes the Scanner object to free up the resources
+        String name = namePrompter.getName();
 
         System.out.printf("Hello, %s. Welcome again to Java Mini Projects.\n", name);
-    }    
+    }
 }
---
+++ src/namegreeter/NamePrompter.java
@@ -0,0 +1,23 @@
+package namegreeter;
+
+import java.util.Scanner;                           // Imports Scanner class
+
+/**
+ * Provides a method to get name from the user
+ */
+public class NamePrompter {
+    /**
+     * Prompts for name from the user and returns it
+     * 
+     * @return The user's name
+     */
+    public String getName() {
+        Scanner scanner = new Scanner(System.in);   // Creates a Scanner object
+
+        System.out.print("What's your name? ");
+        String name = scanner.nextLine();           // Prompts for name from the user
+        scanner.close();                            // Closes the Scanner object to free up the resources
+
+        return name;
+    }
+}
